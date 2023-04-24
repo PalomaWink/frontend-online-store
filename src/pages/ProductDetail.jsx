9892159a744cee23879c10b3c6adc43accde2504
Vitor Gomes <vitorgomes14@hotmail.com>
2023-04-24 19:08:00 -0300

    Feito o requisito 09

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -15,27 +15,44 @@ class ProductDetail extends Component {
   handleClickCategory = async () => {
     const { match: { params: { id } } } = this.props;
     const result = await getProductById(id);
-    console.log(result);
     this.setState({
       productsCategory: result,
     });
+  };
+
+  handleAddToCart = (product) => {
+    const carrinho = JSON.parse(localStorage.getItem('productsList'));
+    product.qtd = 1;
+    if (!carrinho) {
+      const listProducts = JSON.stringify([product]);
+      return localStorage.setItem('productsList', listProducts);
+    }
+    const listProducts = JSON.stringify([...carrinho, product]);
+    return localStorage.setItem('productsList', listProducts);
   };
 
   render() {
     const { productsCategory } = this.state;
     return (
       <div>
-        <h1>Detalhes do Produto</h1>
-        <h2 data-testid="product-detail-name">{productsCategory.title}</h2>
-        <img
-          data-testid="product-detail-image"
-          src={ productsCategory.thumbnail }
-          alt={ productsCategory.title }
-        />
-        <p data-testid="product-detail-price">{productsCategory.price}</p>
+        <div>
+          <h1>Detalhes do Produto</h1>
+          <h2 data-testid="product-detail-name">{productsCategory.title}</h2>
+          <img
+            data-testid="product-detail-image"
+            src={ productsCategory.thumbnail }
+            alt={ productsCategory.title }
+          />
+          <p data-testid="product-detail-price">{productsCategory.price}</p>
+          <button
+            data-testid="product-detail-add-to-cart"
+            onClick={ () => this.handleAddToCart(productsCategory) }
+          >
+            Adicionar ao Carrinho
+          </button>
+        </div>
         <Link
           to="/carrinho"
-          data-testid="shopping-cart-button"
         >
           <button data-testid="shopping-cart-button">Carrinho</button>
         </Link>
