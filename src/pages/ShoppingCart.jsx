aceb92b6c51f615747d430c3cfe3acf6bacbee88
Paloma Wink <paloma.wink@hotmail.com>
2024-02-09 20:06:23 -0300

    style: Estilizando Carrinho de compras

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { CiCirclePlus, CiCircleMinus } from 'react-icons/ci';
+import { BsTrash } from 'react-icons/bs';
+import { TiArrowBack } from 'react-icons/ti';
+
+import '../styles/ShoppingCart.css';
 
 class ShoppingCart extends Component {
   state = {
@@ -51,45 +56,71 @@ class ShoppingCart extends Component {
   render() {
     const { recLocalStorage } = this.state;
     return (
-      <div>
-        {recLocalStorage === null || recLocalStorage.length === 0
-          ? (
-            <p
-              data-testid="shopping-cart-empty-message"
+      <div className="container__main__shopping__cart">
+        <div className="back__home">
+          <Link to="/">
+            <TiArrowBack color="green" size={ 35 } />
+            Voltar
+          </Link>
+        </div>
+        <div className="container__shopping__cart">
+          <div className="title__shopping__cart">
+            <h1>Carrinho de Compras</h1>
+          </div>
+          {recLocalStorage === null || recLocalStorage.length === 0
+            ? (
+              <p
+                data-testid="shopping-cart-empty-message"
+                className="empty__message"
+              >
+                Seu carrinho está vazio
+              </p>
+            )
+            : (recLocalStorage.map((product) => (
+              <div key={ product.id } className="shopping__products__list">
+                <h4 data-testid="shopping-cart-product-name">{product.title}</h4>
+                <img src={ product.thumbnail } alt={ product.title } />
+                <p>
+                  R$
+                  {product.price}
+                </p>
+                <div className="shopping__products__quantity">
+                  <button
+                    className="increase-button"
+                    data-testid="product-increase-quantity"
+                    onClick={ () => this.increaseProduct(product) }
+                  >
+                    <CiCirclePlus size={ 28 } />
+                  </button>
+                  <p data-testid="shopping-cart-product-quantity">{product.qtd}</p>
+                  <button
+                    className="decrease-button"
+                    data-testid="product-decrease-quantity"
+                    onClick={ () => this.decreaseProduct(product) }
+                  >
+                    <CiCircleMinus size={ 28 } />
+                  </button>
+                  <button
+                    data-testid="remove-product"
+                    className="remove-button"
+                    onClick={ () => this.removeProduct(product) }
+                  >
+                    <BsTrash size={ 25 } />
+                  </button>
+                </div>
+              </div>
+            )))}
+          <Link
+            to="/checkout"
+          >
+            <button
+              className="finish__button"
+              data-testid="checkout-products"
             >
-              Seu carrinho está vazio
-            </p>
-          )
-          : (recLocalStorage.map((product) => (
-            <div key={ product.id }>
-              <p data-testid="shopping-cart-product-name">{product.title}</p>
-              <p data-testid="shopping-cart-product-quantity">{product.qtd}</p>
-              <p>{product.price}</p>
-              <button
-                data-testid="product-increase-quantity"
-                onClick={ () => this.increaseProduct(product) }
-              >
-                +
-              </button>
-              <button
-                data-testid="product-decrease-quantity"
-                onClick={ () => this.decreaseProduct(product) }
-              >
-                -
-              </button>
-              <button
-                data-testid="remove-product"
-                onClick={ () => this.removeProduct(product) }
-              >
-                Remover produto
-              </button>
-            </div>
-          )))}
-        <Link
-          to="/checkout"
-        >
-          <button data-testid="checkout-products">Finalizar compra</button>
-        </Link>
+              Finalizar compra
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
